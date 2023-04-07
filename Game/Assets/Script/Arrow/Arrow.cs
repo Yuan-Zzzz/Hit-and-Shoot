97cefd825f5d8d3d9f9bb0294ad099d6372687d8
shuaige <1729726526@qq.com>
2023-04-07 16:22:19 +0800

    反弹前的箭头提示

--- Game/Assets/Script/Arrow/Arrow.cs
+++ Game/Assets/Script/Arrow/Arrow.cs
@@ -3,19 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DirCircle : MonoBehaviour
+public class Arrow : MonoBehaviour
 {
+  
+
+   
     void Update()
     {
+
         if (InputManager.ShootPress)
         {
-            transform.DOScale(new Vector2(1.2f, 1.2f), 0.05f);
+            transform.DOScale(new Vector2(1f, 1f), 0.05f);
         }
-        if(InputManager.ShootRelease) transform.DOScale(new Vector2(1f, 1f), 0.1f);
+        if (InputManager.ShootRelease) transform.DOScale(new Vector2(0f, 0f), 0.1f);
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(InputManager.MousePos);
-        Vector2 direction = mousePos - (Vector2)transform.position;
+        Vector2 direction = -(mousePos - (Vector2)transform.position);
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
-
 }
