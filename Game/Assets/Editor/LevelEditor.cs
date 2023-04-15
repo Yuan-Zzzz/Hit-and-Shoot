e56d6cd809ac79f15ec850b6e597e4913facda01
shuaige <1729726526@qq.com>
2023-04-15 21:24:35 +0800

    继续完善（存在问题：事件注册一个？？）

--- Game/Assets/Editor/LevelEditor.cs
+++ Game/Assets/Editor/LevelEditor.cs
@@ -16,8 +16,8 @@ public class LevelEditor : EditorWindow
     private const float levelHeight = 10;
     private const float levelWidth = 17;
 
-    LevelData_SO selectedLevel;
-
+    public static LevelData_SO selectedLevel;
+    public static SingleBrickData currentBrick;
     [MenuItem("Tools/LevelEditor")]
     public static void ShowMyEditor()
     {
@@ -27,7 +27,7 @@ public class LevelEditor : EditorWindow
     }
     public void CreateGUI()
     {
-      
+
         //加载关卡数据
         var allObjectGuids = AssetDatabase.FindAssets("t:LevelData_SO");
         var allObjects = new List<LevelData_SO>();
@@ -55,96 +55,96 @@ public class LevelEditor : EditorWindow
     }
     private void OnLevelSelectionChange(IEnumerable<object> _selectedLevels)
     {
-        
+
         //清除右侧内容
         rightPane.Clear();
 
         // Get the selected sprite
-         selectedLevel = _selectedLevels.First() as LevelData_SO;
+        selectedLevel = _selectedLevels.First() as LevelData_SO;
         if (selectedLevel == null) return;
+
 
         //绘制容器
         for (int i = 0; i < levelWidth; i++)
         {
             for (int j = 0; j < levelHeight; j++)
             {
-                Button holderButton = new Button(OnHolderClicked);
+                Button holderButton = new Button();
+
                 holderButton.style.position = Position.Absolute;
                 holderButton.text = "Holder";
-                holderButton.style.height =brickSize;
+                holderButton.style.height = brickSize;
                 holderButton.style.width = brickSize;
                 holderButton.style.left = i * brickSize;
                 holderButton.style.top = j * brickSize;
                 rightPane.Add(holderButton);
 
-                //绘制砖块
-                for (int k = 0; k < selectedLevel.bricks.Count; k++)
+
+                holderButton.clicked += () =>
                 {
-                    if (selectedLevel.bricks[k].pos.x+(int)levelWidth/2== i && selectedLevel.bricks[k].pos.y-(int)levelHeight/2 == -j)
+                   // currentBrick = brick;
+                    Debug.Log(new Vector2(i,j));
+                };
+                //绘制砖块
+                foreach (var brick in selectedLevel.bricks)
+                {
+
+
+                    if (brick.pos.x + (int)levelWidth / 2 == i && brick.pos.y - (int)levelHeight / 2 == -j)
                     {
                         var spriteImage = new Image();
                         spriteImage.scaleMode = ScaleMode.ScaleToFit;
-                        spriteImage.sprite = selectedLevel.bricks[k].brick.gameObject.GetComponent<SpriteRenderer>().sprite;
-                        spriteImage.tintColor = new Color(selectedLevel.bricks[k].data.brickColor.r, selectedLevel.bricks[k].data.brickColor.g, selectedLevel.bricks[k].data.brickColor.b, 1f);
+                        spriteImage.sprite = brick.brick.gameObject.GetComponent<SpriteRenderer>().sprite;
+                        spriteImage.tintColor = new Color(brick.data.brickColor.r, brick.data.brickColor.g, brick.data.brickColor.b, 1f);
                         holderButton.Add(spriteImage);
                     }
+
+                   
                 }
             }
         }
-
     }
 
-    private void OnHolderClicked()
+
+
+private void OnHolderClicked()
+{
+    var window = new BrickEditorWindow();
+    window.ShowModal();
+}
+
+public class BrickEditorWindow : EditorWindow
+{
+
+
+    private void OnEnable()
     {
-        var window = new BrickEditorWindow();
-        window.ShowModal();
 
 
+        rootVisualElement.Add(new Label("编辑砖块"));
+
+        //Color test = selectedLevel.
+
+        //var gameObjectBox = new Box();
+        //gameObjectBox.Add(new Label("砖块预制体"));
+        //gameObjectBox.Add(new ObjectField());
+        //rootVisualElement.Add(gameObjectBox);
+        //var colorBox = new Box();
+        //colorBox.Add(new Label("砖块颜色"));
+        //colorBox.Add(new ColorField());
+        //rootVisualElement.Add(colorBox);
+
+        //var countBox = new Box();
+        //countBox.Add(new Label("受击次数"));
+        //countBox.Add(new IntegerField());
+        //rootVisualElement.Add(countBox);
+
+        //var riftBox = new Box();
+        //riftBox.Add(new Label("分裂数"));
+        //riftBox.Add(new IntegerField());
+        //rootVisualElement.Add(riftBox);
     }
 
-    public class BrickEditorWindow : EditorWindow
-    {
-        TextField m_ObjectNameBinding;
-        
-        private void OnEnable()
-        {
-            var label = new Label("Hello World!");
-            
-            rootVisualElement.Add(label);
 
-        }
-        public void CreateGUI()
-        {
-            m_ObjectNameBinding = new TextField("Object Name Binding");
-            // Note: the "name" property of a GameObject is "m_Name" in serialization.
-            m_ObjectNameBinding.bindingPath = "m_Name";
-            rootVisualElement.Add(m_ObjectNameBinding);
-            OnSelectionChange();
-        }
-
-        public void OnSelectionChange()
-        {
-            GameObject selectedObject = Selection.activeObject as GameObject;
-            if (selectedObject != null)
-            {
-                // Create the SerializedObject from the current selection
-                SerializedObject so = new SerializedObject(selectedObject);
-                // Bind it to the root of the hierarchy. It will find the right object to bind to.
-                rootVisualElement.Bind(so);
-
-                // Alternatively you can instead bind it to the TextField itself.
-                // m_ObjectNameBinding.Bind(so);
-            }
-            else
-            {
-                // Unbind the object from the actual visual element that was bound.
-                rootVisualElement.Unbind();
-                // If you bound the TextField itself, you'd do this instead:
-                // m_ObjectNameBinding.Unbind();
-
-                // Clear the TextField after the binding is removed
-                m_ObjectNameBinding.value = "";
-            }
-        }
-    }
+}
 }
