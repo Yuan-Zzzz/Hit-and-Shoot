481081c4bae013b25ca01ef3714fc25cd30f6d43
shuaige <1729726526@qq.com>
2023-05-09 08:43:08 +0800

    冲击波特效结束

--- Game/Assets/Script/ShockWave/ShockWaveController.cs
+++ Game/Assets/Script/ShockWave/ShockWaveController.cs
@@ -15,18 +15,19 @@ public class ShockWaveController : MonoBehaviour
     private void OnExitBulletTime()
     {
 
-        StartCoroutine(Wave(spriteRenderer));
+      
         var ball = GameObject.FindObjectOfType<BallController>().gameObject;
         transform.position = ball.transform.position;
         spriteRenderer.material.SetFloat("_WaveDistanceFromCenter", -0.1f);
+        StartCoroutine(Wave(spriteRenderer));
     }
     IEnumerator Wave(SpriteRenderer waveSpr)
     {
 
         while (waveSpr.material.GetFloat("_WaveDistanceFromCenter") < 1)
         {
-            waveSpr.material.SetFloat("_WaveDistanceFromCenter", waveSpr.material.GetFloat("_WaveDistanceFromCenter") + 0.035f);
-            yield return new WaitForEndOfFrame();
+            waveSpr.material.SetFloat("_WaveDistanceFromCenter", waveSpr.material.GetFloat("_WaveDistanceFromCenter") + 0.02f);
+            yield return new WaitForSeconds(0.01f);
         }
 
     }
